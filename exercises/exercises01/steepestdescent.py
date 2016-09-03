39870282f3324d779d9da3f64ad8efb136529b92
Kevin Song <chipbuster@gmail.com>
2016-09-02 21:17:25 -0500

    Refactor steepest descent code into sep module

--- exercises/exercises01/steepestdescent.py
+++ exercises/exercises01/steepestdescent.py
@@ -14,8 +14,10 @@ def safe_exp(val):
     be too large, it replaces it with a safe value."""
 
     if val > largest_safe_exponent:
+        print("[WARN]: Exponential term was capped to avoid overflow. This may cause divergence.")
         return math.exp(largest_safe_exponent)
     elif val < smallest_safe_exponent:
+        print("[WARN]: Exponential term was capped to avoid underflow. This may cause divergence.")
         return math.exp(smallest_safe_exponent)
     else:
         return math.exp(val)
@@ -62,7 +64,7 @@ def calc_grad_function(X,y,m):
 
     return grad
 
-def steepest_descent(params, initial_guess, converge_step):
+def solve(params, initial_guess, converge_step):
 
     (X,y,m) = params
 
@@ -94,46 +96,3 @@ def steepest_descent(params, initial_guess, converge_step):
         print(delta)
 
     return (guess,likelihood_record)
-
-def main(filename):
-    """Driver for steepest descent code."""
-
-    rawdata = []
-
-    # Read the data in to rawdata
-    with open(filename,'r') as ifile:
-        r = csv.reader(ifile)
-        for row in r:
-            rawdata.append(row)
-
-    # Get the data columns from the raw data and convert to matrix
-    datalist = [ row[2:11] for row in rawdata ]
-    predictors = np.array(datalist)
-    predictors = predictors.astype(float)
-
-    # Attach column of ones for intercept term
-    onecol = np.matrix(np.ones(np.shape(predictors)[0])).T
-
-    predictors = np.matrix(np.concatenate((predictors, onecol),axis=1))
-
-    # Get the responses and convert to 0-1 matrix
-    responselist = [ 1 if row[1] == "M" else 0 for row in rawdata ]
-    response = np.matrix(responselist).T
-
-    # Get the "trial number" for the MLE, in this case, a vectors of 1s
-    trials = np.matrix(np.ones(np.shape(response)))
-
-    # Recondition matrix to have order of magnitude ~1
-    condWeights = np.average(predictors,axis=0)
-    W = np.matrix(np.diagflat(condWeights))
-
-    predictors = predictors * W.I
-
-    initGuess = np.matrix(np.ones((np.shape(predictors)[1],1))) * 0.001
-
-    solution,_ = steepest_descent((predictors, response, trials), initGuess, 0.01)
-
-    # Since we changed the predictors, need to reverse-transform the solution
-    solution = W * solution
-
-    print(solution)
