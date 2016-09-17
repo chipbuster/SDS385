d7b10b22a56ca817e6257002a6862538e409225f
Kevin Song <chipbuster@gmail.com>
2016-09-16 22:05:24 -0500

    Newton method testing

--- exercises/exercises01/optimization/newtonmethod.py
+++ exercises/exercises01/optimization/newtonmethod.py
@@ -1,10 +1,13 @@
 import numpy as np
 from scipy import linalg as spla
+from scipy import special as spsp
 from numpy import linalg as npla
 import sys
 import math
 import os
 from copy import copy
+from sklearn.preprocessing import scale
+import pdb
 
 sys.path.append(os.path.join(os.path.dirname(__file__),'..','common'))
 import logistic_common as lc
@@ -41,11 +44,37 @@ def gen_hessian_function(X,y,m):
 
         D = np.diagflat(np.array(d_elem))
 
-        hess = X.T * D * X
+        hess = np.matrix(X.T) * np.matrix(D) * np.matrix(X)
 
         return hess
 
     return calc_Hess
+
+def gen_D_mat(X,y,m,B):
+
+    (N,P) = np.shape(X)
+
+    weights = [ lc.calc_weight(x,B) for x in X ]
+    oneMinusW = [ 1 - w for w in weights ]
+
+    d_elem = [None] * N
+    for j in range(len(weights)):
+        d_elem[j] = np.asscalar(m[j]) * weights[j] * oneMinusW[j]
+
+    D = np.matrix(np.diagflat(np.array(d_elem)))
+    return D
+
+def gen_S_vec(X,y,m,B):
+
+    (N,P) = np.shape(X)
+
+    exp = np.dot(X,B)
+    w = spsp.expit(exp)
+
+    W = np.diagflat(w)
+
+    coeffs = np.matrix( W * m - y )
+    return coeffs
 
 def solve(params, initial_guess, converge_step):
 
@@ -72,24 +101,28 @@ def solve(params, initial_guess, converge_step):
         oldLLVal = LLVal
         oldGuess = guess
 
-        grad = grad_func(guess)
-        hess = hess_func(guess)
 
-        # Solve for search direction
+#  Alternate solution method
+#        D = gen_D_mat(X,y,m,guess)
+#        s = gen_S_vec(X,y,m,guess)
+#        Z = X * guess - D.I * s
+#        searchDir = lc.solve_wls(D,X,Z)
 
-        searchDir = spla.solve(hess , grad)
-
-        assert npla.norm(hess * searchDir - grad) < 0.1, "Bad solve"
-
-        print(searchDir)
-
-        guess = guess - searchDir * 0.1
 
         # Calculate current likelihood for convergence determination
         LLVal = llh_func(guess)
         delta = abs( oldLLVal - LLVal )
 
         likelihood_record.append(LLVal)
+
+        hess = hess_func(guess)
+        grad = grad_func(guess)
+        searchDir = spla.solve(hess,grad)
+
+        print("Condition number of Hessian is " + str(npla.cond(hess)))
+        print(hess)
+
+        guess = guess + searchDir
 
         # Update the user and break out if needed
         iterct += 1
@@ -104,11 +137,10 @@ def main(csvfile):
     # Assume that csvfile points to wdbc.csv
     np.random.seed(5)
 
-    (X,y,m) = dc.process_wdbc(csvfile)
-    W = dc.prescaling_matrix(X)
+    (X1,y,m) = dc.process_wdbc(csvfile)
 
     # Scale the predictors
-    X = X * W.I
+    X = scale(X1,axis=1)
 
     # Generate initial guess
     numParams = np.shape(X)[1]
