6003081f260fb0ddc314a6b56422d195b12c5018
Kevin Song <chipbuster@gmail.com>
2016-11-14 10:38:11 -0600

    Stuff!

--- exercises/exercises09/code/pmf.py
+++ exercises/exercises09/code/pmf.py
@@ -5,18 +5,20 @@ import scipy as sp
 import math
 import random
 import time
+import re
 import numpy.linalg as npla
 import scipy.sparse as spsp
 import scipy.sparse.linalg as spspla
 from scipy.sparse.linalg import spsolve
 from numpy import inf
 from copy import deepcopy
+from sklearn.preprocessing import scale
 
 import pdb
 
 def normalize_l2(x):
     """Normalize the vector x in the L2 norm"""
-    return x / math.sqrt(np.norm(x))
+    return x / (npla.norm(x))
 
 def softmax(a,c):
     """Compute the elementwise softmax of two vectors"""
@@ -67,9 +69,12 @@ def update_with_delta(Xx, maxNorm):
     # Define the boundaries of the search area
     dMin = 1e-6
     d = (dMax + dMin) / 2
+    delta = constvec(d)
+    x = softmax(Xx, delta)
     l1x = l1(x)
 
     while abs(l1x - maxNorm) > 1e-5:
+#        print("update w/ d = " + str(d) + " l1x = " + str(l1(x)) )
         # Update guess of d
         if l1x > maxNorm:
             dMin = d
@@ -101,28 +106,28 @@ def compute_factor(X, v, c1, c2):
 
     assert sz_v == np.size(v)
 
-    u = update_with_delta(X @ v)
-    v = update_with_delta(X.T @ u)
+    u = update_with_delta(X @ v, c1)
+    v = update_with_delta(X.T @ u, c2)
 
     delta_u = 1000
     delta_v = 1000
 
-    while delta_u > 1e-5 and delta_v > 1e-5:
+    while delta_u > 1e-5 or delta_v > 1e-5:
         oldU = u
         oldV = v
 
         u = update_with_delta(X @ v, c1)
         v = update_with_delta(X.T @ u, c2)
 
-        delta_u = np.norm(u - oldU) / sz_u
-        delta_u = np.norm(v - oldV) / sz_v
+        delta_u = npla.norm(u - oldU) / sz_u
+        delta_v = npla.norm(v - oldV) / sz_v
 
     d = u.T @ X @ v
 
     return (d,u,v)
 
-def compute_pmf(X, rank, c1, c2):
-    """Compute the penalized matrix factorization of X.
+def compute_pmf(X_inp, rank, c1, c2):
+    """Compute the penalized matrix fact_inporization of X.
 
     Finds set of U vectors and V vectors such that
       || X - duv.t ||_F is minimized
@@ -132,24 +137,91 @@ def compute_pmf(X, rank, c1, c2):
     In other words, subject to some sparsity and norm constraints, we should
     have that the sum over i of (d_i * u_i * v_i.T) is close to X.
 
-    X: (np.array)  -- The matrix to be factorized
-    rank: (scalar) -- The number of u/v vectors to extract
-    c1:   (scalar) -- The L1 bound on u vectors
-    c2:   (scalar) -- The L1 bound on v vectors
-    """
+    X_inp: (np.array)  -- The matrix to be factorized
+    rank:     (scalar) -- Th_inpe number of u/v vectors to extract
+    c1:       (scalar) -- The L1     bound on u vectors
+    c2:       (scalar) -- The L1     bound on v vectors
+        """
 
     X_arr = []
     u_arr = []
     v_arr = []
     d_arr = []
 
+    X = deepcopy(X_inp)
+
     v_init = np.ones((np.shape(X)[1],1))
 
     for i in range(rank):
         X_arr.append(X)
         (d,u,v) = compute_factor(X, v_init, c1, c2)
+
+
+        assert abs(npla.norm(u) - 1 ) < 1e-3
+        assert abs(npla.norm(v) - 1 ) < 1e-3
+
         d_arr.append(d)
         u_arr.append(u)
         v_arr.append(v)
 
-        X -= d * u @ v.T
+        toSub = np.outer(u,v)
+        assert np.shape(toSub) == np.shape(X)
+        X -= d * toSub
+
+    return (X_arr, u_arr, v_arr, d_arr)
+
+def main():
+    fname = "social_marketing.csv"
+    cols = tuple(range(1,37))
+
+    rawdata = np.loadtxt(fname, delimiter=',', skiprows=1, usecols=cols)
+
+    assert np.shape(rawdata)[1] == 36
+
+    data = rawdata
+    maxrank = 5
+
+    (Xs,us,vs,ds) = compute_pmf(data, maxrank, 200, 200)
+
+    X_approx = np.zeros(np.shape(data))
+    for j in range(maxrank):
+        d = ds[j]
+        u = us[j]
+        v = vs[j]
+
+        X_approx += d * u @ v.T
+        err = npla.norm(X_approx - data)
+
+        print(err)
+
+    # Sanity check: how well does SVD do?
+    # Ans: similar enough, but really slow
+
+    #(U,S,V) = npla.svd(data)
+    #Z = np.zeros(np.shape(data))
+    #for j in range(maxrank):
+    #    d = S[j]
+    #    u = U[:,j]
+    #    v = V[j,:]
+
+    #    Z += d * np.outer(u,v)
+
+    #    err = npla.norm(Z - data)
+    #    print(err)
+
+    with open(fname, 'r') as infile:
+        first = infile.readline()
+
+    noquot = re.sub('\"',"",first)
+    topics = noquot.split(',')[1:]
+
+    i = 0
+    for v in vs:
+        i += 1
+        print("\nSingular Vector #" + str(i))
+        for j in range(len(v)):
+            if v[j] > 1e-10:
+                print(v[j], topics[j])
+
+if __name__ == '__main__':
+    main()
